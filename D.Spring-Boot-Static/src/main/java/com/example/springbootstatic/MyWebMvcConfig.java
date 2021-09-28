e970662e471173a19aaa6478a39329ebf30f9846
codecodercoding <kneil5778@gmail.com>
2021-09-28 13:56:32 +0100

    更新

---
+++ D.Spring-Boot-Static/src/main/java/com/example/springbootstatic/MyWebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.example.springbootstatic;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author CodeCoderCoding
+ */
+@Configuration
+public class MyWebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
