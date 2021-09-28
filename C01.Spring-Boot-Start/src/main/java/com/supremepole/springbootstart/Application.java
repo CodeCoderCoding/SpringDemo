588d10b259e91741c7cc835ef2164eafc55fe7bd
codecodercoding <kneil5778@gmail.com>
2021-09-28 01:46:01 +0100

    init

---
+++ C01.Spring-Boot-Start/src/main/java/com/supremepole/springbootstart/Application.java
@@ -0,0 +1,13 @@
+package com.supremepole.springbootstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
